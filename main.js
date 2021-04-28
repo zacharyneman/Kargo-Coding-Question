78a49824e1cb666833dbb9d382d48845dd4ac4c2
Zachary Neman <zacharyneman@Zacharys-MacBook-Pro-2.local>
2021-04-28 12:47:29 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+function numToPhonetic(){
+    var a = process.argv.slice(2)
+    var result = ''
+    var numbers = {'0': 'Zero', '1':'One', '2':'Two', '3':'Three', '4':'Four', 
+                '5':'Five','6':'Six', '7':'Seven', '8':'Eight', '9':'Nine'}
+    a.forEach(num => {
+        var temp = ''
+        num.split('').forEach(x => {
+            temp = temp + numbers[x]
+        })
+        result = result + temp + ','
+    })
+    result = result.slice(0,-1) + '\n'
+    process.stdout.write(result)
+}
+
+numToPhonetic([30, 300, 5])
